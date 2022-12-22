5dd1bcfe2f3bb4f9c05d2a229ab194d7948d60ce
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-12-22 10:23:04 +0300

    added solution to file homework02.py

--- homework02.py
+++ homework02.py
@@ -6,10 +6,24 @@
 # [2, 3, 5, 6] => [12, 15]
 
 from random import randint
+from math import ceil
+
+while True:
+    try:
+        n = int(input('Введите количество элементов списка: '))
+        if n > 0:
+            break
+    except:
+        print('Введите целое число!')
 
 my_list = []
-n = 5
 
 for i in range(n):
     my_list.append(randint(1, 9))
-print(f'Заданный список: \n{my_list}')
+
+res_list = []
+for i in range(int(ceil(len(my_list)/2))):
+    item = my_list[i]*my_list[len(my_list) - i - 1]
+    res_list.append(item)
+
+print(f'{my_list} -> {res_list}')
