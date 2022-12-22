5eba65cb6e1ffd7c1e282a9468573fc83eec00f1
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-12-22 13:18:59 +0300

    added file homework04.py

--- homework04.py
+++ homework04.py
@@ -7,9 +7,16 @@
 
 while True:
     try:
-        n = int(input('Введите количество элементов списка: '))
+        n = int(input('Введите число: '))
         if n > 0:
             break
     except:
         print('Введите целое число!')
 
+new_list = []
+
+while n > 0:
+    item = n % 2
+    new_list.append(item)
+    n //= 2
+print(new_list.reverse())
